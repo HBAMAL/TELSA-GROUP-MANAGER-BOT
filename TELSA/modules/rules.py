3002867cdeb7cba6b2e2d90c8d59f1ec0f55a48a
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 18:57:55 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/rules.py
+++ TELSA/modules/rules.py
@@ -104,7 +104,7 @@ __help__ = """
 /clearrules: clear the rules for this chat.
 """
 
-__mod_name__ = "Rules"
+__mod_name__ = "🧑🏻‍⚖️RULES👩🏻‍⚖️"
 
 GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
 SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
